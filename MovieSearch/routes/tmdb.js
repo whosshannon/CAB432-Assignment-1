cc82d3c54774566f0918364d145ac8fbfeb9352a
shannon <shannon.herondale@gmail.com>
2019-08-27 13:41:36 +1000

    Start on the Twitter api
    - currently using Twit node package (unsure if allowed for assignment)
    - no gui for twitter responses

--- MovieSearch/routes/tmdb.js
+++ MovieSearch/routes/tmdb.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const logger = require('morgan');
+const Twit = require('twit');
 const router = express.Router();
 const CONFIG = require("../config.json");
 
 router.use(logger('tiny'));
 
-router.get('/test', (req, res) => {
+router.get('/test/tmdb', (req, res) => {
     const query = req.query;
     const options = createTmbdOptions(query['query']);
 
@@ -26,6 +27,20 @@ router.get('/test', (req, res) => {
             console.error(error);
         })
 
+})
+
+router.get('/test/twitter', (req, res) => {
+    const query = req.query;
+    const options = createTwitterOptions(query['query'], query['count']);
+
+    T.get('search/tweets', options, function(err, data, response) {
+        for (i = 0; i < data.statuses.length; i++) {
+            console.log(data.statuses[i].text);
+        }
+    }) 
+
+    res.write('yeet');
+    res.end(); 
 })
 
 router.get('/full', (req, res) => {
@@ -48,6 +63,13 @@ router.get('/full', (req, res) => {
             console.error(error);
         })
 })
+
+var T = new Twit({
+    consumer_key: CONFIG.twitter_consumer_key,
+    consumer_secret: CONFIG.twitter_consumer_secret,
+    access_token: CONFIG.twitter_access_token,
+    access_token_secret: CONFIG.twitter_access_token_secret,
+  })
 
 const tmdb = {
     api_key: CONFIG.tmdb_api_key,
@@ -73,6 +95,14 @@ function createTmbdOptions(query) {
     return options;
 };
 
+function createTwitterOptions(query, count) {
+    const options = {
+         q: query, 
+         count: count 
+    }
+    return options;
+}
+
 function createPageTMDB(title, rsp) {
     const results = rsp.total_results;
     console.log("results: " + results);
@@ -87,9 +117,12 @@ function createPageTMDB(title, rsp) {
         console.log("item " + item + ": " + item.name + " with ID " + item.id);
         console.log();
 
-        const itemStr = '</br> <a href="https://www.themoviedb.org/person/' + item.id + '"> <div>' +
-        '<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2' + item.profile_path + '" alt= "' + item.id + '" height="200">' +
-        '<p><b>' + item.name + '</b> known for ' + item.known_for_department + ' with id ' + item.id + '</p>'
+        const itemStr = '</br> <a href="https://www.themoviedb.org/person/' 
+        + item.id + '"> <div style="background-color:rgb(250, 250, 250);">' +
+        '<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2' + item.profile_path 
+        + '" alt= "' + item.id + '" height="200">' +
+        '<p><b>' + item.name + '</b> known for ' 
+        + item.known_for_department + ' with id ' + item.id + '</p>'
         + '</div> </a>';
 
         str+= itemStr;
