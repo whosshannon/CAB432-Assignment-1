f718c617fd03f0a07458f5a6c77392f96cf0b587
shannon <shannon.herondale@gmail.com>
2019-08-31 15:22:10 +1000

    Use .pug for home page

--- MovieSearch/app.js
+++ MovieSearch/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 const helmet = require('helmet');
-const tmdbRouter = require('./routes/tmdb');
+const searchRouter = require('./routes/search');
 const newsRouter = require('./routes/news');
 const testRouter = require('./routes/test');
+var path = require('path');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -13,18 +17,11 @@ const port = 3000;
 app.use(helmet());
 
 app.get('/', (req, res) => {
-    res.writeHead(200,{'content-type': 'text/html'});
-
-    fs.readFile('index.html', 'utf8', (err, data) => {
-        if (err) {
-            res.end('Could not find or open file for reading\n');
-        } else {
-            res.end(data);
-        }
-    });
+    //res.writeHead(200,{'content-type': 'text/html'});
+    res.render('index');
 });
 
-app.use('/search',tmdbRouter); 
+app.use('/search',searchRouter); 
 app.use('/news', newsRouter);
 app.use('/test', testRouter);
 
--- MovieSearch/routes/test.js
+++ MovieSearch/routes/test.js
@@ -6,72 +6,16 @@ const CONFIG = require("../config.json");
 
 router.use(logger('tiny'));
 
-router.get('/', (req, res) => {
-    const query = req.query;
-    const options = createTmbdOptions(query['query'], query['year']);
-
-    const url = `https://${options.hostname}${options.path}`;
-    
-    axios.get(url) 
-        .then( (response) => {
-            res.writeHead(response.status, {'content-type': 'text/html'});
-            return response.data;
+/* GET home page. */
+router.get('/', (req, res) => { 
+    const url = 'https://kctbh9vrtdwd.statuspage.io/api/v2/status.json';
+    axios.get(url)
+        .then((response) => { 
+            const rsp = response.data;
+            res.render('index', { rsp });
         })
-        .then ( (rsp) => {
-            const htmlResponse = createPageTMDB(query['query'], rsp);
-            res.write(htmlResponse);
-            res.end();
+        .catch((error) => { 
+            res.render('error', { error });
         })
-        .catch((error) => {
-            console.error(error);
-        })
-})
-
-const tmdb = {
-    api_key: CONFIG.tmdb_api_key,
-    lang: 'en-US',
-    include_adult: 'false'
-}
-
-function createTmbdOptions(query, year) {
-    const options = {
-        hostname: 'api.themoviedb.org',
-        port: 443,
-        path: '/3/search/movie?'
-    }
-    
-    const str = 'api_key=' + tmdb.api_key +
-    '&language=' + tmdb.lang +
-    '&query=' + query +
-    '&year=' + year +
-    '&include_adult=' + tmdb.include_adult;
-    
-    options.path+=str;
-    return options;
-};
-
-function createPageTMDB(title, rsp) {
-    
-    let str = '<!DOCTYPE html>' +
-    '<html><head><title>TBDM</title></head>' +
-    '<body>' +
-    '<h1>Search results for: "' + title + '"</h1>';
-    for (let counter in rsp.results) {
-        const item = rsp.results[counter];
-
-        const itemStr = '</br> <a href="http://127.0.0.1:3000/news?id='
-        + item.id +  '"> <div style="background-color:rgb(250, 250, 250);">' +
-        '<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2' + item.poster_path 
-        + '" alt= "' + item.id + '" height="200">' 
-        +'<p><b>' + item.title + '</b></p><p>Released date: ' + item.release_date + '</p><p>'  
-        + item.overview + '</p>'
-        + '</div> </a>';
-
-        str+= itemStr;
-    }
-    str += '</body></html>';
-
-    return str;
-}
-
+    }); 
 module.exports = router;
